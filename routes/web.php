353db78bfe69477d594e1323e7a106dc4847eba8
AfiaAkterMunni <afiamunni03@gmail.com>
2023-01-08 01:09:25 +0600

    all posts dashboard page design.

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\PostController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -13,10 +14,8 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
-
 Auth::routes();
 
-Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+Route::middleware('auth')->group(function(){
+    Route::get('/posts', [PostController::class, 'index'])->name('posts');
+});
